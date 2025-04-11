bca147e7c73eb6d48a4258da617bb92e8f1ab59a
amolshende2507 <amolshende66151@gmail.com>
2025-04-11 13:49:52 +0530

    added basic styling and image and description

--- models/campground.js
+++ models/campground.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title:String,
-    price:String,
-    description :String,
-    location :String,
+    image:String,
+    price:Number,
+    description:String,
+    location:String,
 });
 
 module.exports = mongoose.model('CampGround',CampgroundSchema) ;
