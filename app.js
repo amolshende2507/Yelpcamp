c4d50a40dd5bf3b7c25ab6a5529574d5f49becf2
amolshende2507 <amolshende66151@gmail.com>
2025-07-21 23:48:28 +0530

    just arranging

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use((req, res, next) => {
 })
 
 
-app.use('/',userRoutes)
+app.use('/', userRoutes)
 app.use('/campgrounds', campgroundRoutes)
 app.use('/campgrounds/:id/reviews', reviewRoutes)
 
