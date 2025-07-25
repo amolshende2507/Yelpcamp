6087fa21f83d54f6f9d558ccfd03e814e52c5cda
amolshende2507 <amolshende66151@gmail.com>
2025-07-25 12:04:48 +0530

    updated logout in user.js

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 
 module.exports.renderRegister = (req, res) => {
     res.render('users/register');
-}
+};
 
 module.exports.register = async (req, res, next) => {
     try {
@@ -13,26 +13,31 @@ module.exports.register = async (req, res, next) => {
             if (err) return next(err);
             req.flash('success', 'Welcome to Yelp Camp!');
             res.redirect('/campgrounds');
-        })
+        });
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('register');
     }
-}
+};
 
 module.exports.renderLogin = (req, res) => {
     res.render('users/login');
-}
+};
 
 module.exports.login = (req, res) => {
-    req.flash('success', 'welcome back!');
+    req.flash('success', 'Welcome back!');
     const redirectUrl = req.session.returnTo || '/campgrounds';
     delete req.session.returnTo;
     res.redirect(redirectUrl);
-}
+};
 
-module.exports.logout = (req, res) => {
-    req.logout();
-    req.flash('success', "Goodbye!");
-    res.redirect('/campgrounds');
-}
+// ✅ Corrected logout function
+module.exports.logout = (req, res, next) => {
+    req.logout(function(err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', "Goodbye!");
+        res.redirect('/campgrounds');
+    });
+};
