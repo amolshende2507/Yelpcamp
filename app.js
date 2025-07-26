5de5d918d47a63d6c5d90d53ca6743128eaef8e3
amolshende2507 <amolshende66151@gmail.com>
2025-07-26 13:47:48 +0530

    added images using cloudinary

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+if(process.env.NODE_ENV !=="production"){
+    require('dotenv').config();
+}
+
+
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -16,8 +22,8 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  
+  useUnifiedTopology: true,
 });
 
 
