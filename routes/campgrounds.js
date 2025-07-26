7d68e0963f1ee07f5d7b85cf0112084eb2551b23
amolshende2507 <amolshende66151@gmail.com>
2025-07-26 17:17:52 +0530

    finish image upload and thumbnail

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,8 +18,15 @@ router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .put(
+        isLoggedIn,
+        isAuthor,
+        upload.array('image'),        // ✅ must come before Joi validation
+        validateCampground,
+        catchAsync(campgrounds.updateCampground)
+    )
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
 
